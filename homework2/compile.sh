4f1f33ced197544946f5b31fb4cf286e48fcb47a
Al8a <35094272+Al8a@users.noreply.github.com>
2022-10-31 10:59:55 -0600

    Added Makefile and compile script.

---
+++ homework2/compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+
+# This bash script is used to help implement and test our implementation files.
+rm *o *.so  # Remove any files beforehand
+
+gcc -fPIC -Wall -g -O0 -c memory.c
+gcc -fPIC -Wall -g -O0 -c implementation.c
+gcc -fPIC -shared -o memory.so memory.o implementation.o -lpthread
+
+export LD_LIBARY_PATH=`pwd`:"$LD_LIBRARY_PATH"
+export LD_PRELOAD=`pwd`/memory.so
+export MEMORY_DEBUG=yes
+
+
+
